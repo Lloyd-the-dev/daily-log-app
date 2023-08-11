4a4299521815858ed563a694ca7855119018c20d
Favour Ojo <ojoore35@gmail.com>
2023-08-11 11:28:17 +0100

    login and signup functioning well

--- login.php
+++ login.php
@@ -1,28 +1,23 @@
 <?php 
 
-    $con = new mysqli("localhost","root","","daily_logging");
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $email = $_POST["email"];
-            $password = $_POST["password"];
+    $con = mysqli_connect("localhost","root","","daily_logging");
+   
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
 
-            $sql = "SELECT user_id, Fullname, Email, Password FROM user_details WHERE Email = '$email'";
-            $result = $con->query($sql);
-        
-            if ($result->num_rows == 1) {
-                $row = $result->fetch_assoc();
-                if (password_verify($password, $row["Password"])) {
-                    session_start();
-                    $_SESSION["user_id"] = $row["user_id"];
-                    $_SESSION["email"] = $row["Email"];
-                    header("Location: dashboard.php"); // Redirect to user dashboard
-                } else {
-                    echo "Invalid password.";
-                }
-            } else {
-                echo "Invalid Email."; 
-            }
-        }
+    $sql = "SELECT user_id, Fullname, Email, Password FROM user_details WHERE Email = '$email' AND Password = '$password'";
+    $result = mysqli_query($con, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+    $count = mysqli_num_rows($result);  
+      
+    if($count == 1){  
+        echo "<h1><center> Login successful </center></h1>";  
+    }  
+    else{  
+        echo "<h1> Login failed. Invalid username or password.</h1>";  
+    }     
+
     $con->close();
       
 ?>
