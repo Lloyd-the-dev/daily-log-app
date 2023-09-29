9be31a224ffeb54905fb51990acbaa3c56ad4844
Favour Ojo <ojoore35@gmail.com>
2023-09-29 10:25:58 +0100

    minor tweaks

--- dashboard.php
+++ dashboard.php
@@ -34,7 +34,7 @@
     <style id="table_style" type="text/css">
         body {
             min-height: 100vh;
-            background: url(https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/844a2e12-df15-4697-b172-3e05db4d3413) no-repeat;
+            background: url(https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/844a2e12-df15-4697-b172-3e05db4d3413), url("./images/dark-background.jpg") no-repeat;
             background-size: cover;
             background-position: center;
             color: #fff;
